5d945a415dd962b0bf2e183cbb2c6a77de5a35d5
Oltorc <kolkien2003@gmail.com>
2026-02-28 18:57:59 +0100

    Update save/load system to use JSON format
    Refactor save/load system to use JSON instead of pickle and add save/load functions for game state.

--- save_load.py
+++ save_load.py
@@ -1,37 +1,63 @@
-import pickle
-import os
+SAVE_FILE = "savegame.json"
 
-class SaveLoadSystem:
-    def __init__(self, file_extension, save_folder):
-        self.file_extension = file_extension
-        self.save_folder = save_folder
-    
-    def save_data(self, data, name):
-        data_file = open(self.save_folder + "/" + name + self.file_extension, "wb")
-        pickle.dump(data, data_file)
-        
-    def load_data(self, name):
-        data_file = open(self.save_folder + "/" + name + self.file_extension, "rb")
-        data = pickle.load(data_file)
-        return data
-    
-    def check_for_file(self, name):
-        return os.path.exists(self.save_folder + "/" + name + self.file_extension)
-    
-    def load_game_data(self, files_to_load, default_data):
-        variables = []
-        for index, file in enumerate(files_to_load):
-            if self.check_for_file(file):
-                variables.append(self.load_data*(file))
-            else:
-                variables.append(default_data[index])
-                
-            
-        if len(variables) > 1:
-            return tuple(variables)
-        else:
-            return variables[0]
-        
-    def save_game data(self, data_to_save, file_name)
-        for index, file in enumerate(data_to_save):
-            if self.save_data()
+
+def sauvegarder_partie():
+    global nombre_journees, niveau, xp, xp_for_level
+    global player_x, player_y, pv_joueur, pv_max_joueur
+    global upgrades_joueur, type_armes
+
+    data = {
+        "nombre_journees": nombre_journees,
+        "niveau": niveau,
+        "xp": xp,
+        "xp_for_level": xp_for_level,
+        "player_x": player_x,
+        "player_y": player_y,
+        "pv_joueur": pv_joueur,
+        "pv_max_joueur": pv_max_joueur,
+        "upgrades_joueur": upgrades_joueur,
+        "armes_debloquees": [arme.classe.__name__ if hasattr(arme, "classe") else "aura" for arme in type_armes]
+    }
+
+    with open(SAVE_FILE, "w") as f:
+        json.dump(data, f)
+
+    print("Partie sauvegardée !")
+
+
+def charger_partie():
+    global nombre_journees, niveau, xp, xp_for_level
+    global player_x, player_y, pv_joueur, pv_max_joueur
+    global upgrades_joueur, type_armes
+    global laser, roquette, mine, aura_active
+
+    try:
+        with open(SAVE_FILE, "r") as f:
+            data = json.load(f)
+
+        nombre_journees = data["nombre_journees"]
+        niveau = data["niveau"]
+        xp = data["xp"]
+        xp_for_level = data["xp_for_level"]
+        player_x = data["player_x"]
+        player_y = data["player_y"]
+        pv_joueur = data["pv_joueur"]
+        pv_max_joueur = data["pv_max_joueur"]
+        upgrades_joueur = data["upgrades_joueur"]
+
+        type_armes.clear()
+
+        for arme_nom in data["armes_debloquees"]:
+            if arme_nom == "projectile_laser":
+                type_armes.append(laser)
+            elif arme_nom == "projectile_roquette":
+                type_armes.append(roquette)
+            elif arme_nom == "projectile_mine":
+                type_armes.append(mine)
+            elif arme_nom == "aura":
+                type_armes.append(aura_active)
+
+        print("Partie chargée !")
+
+    except FileNotFoundError:
+        print("Aucune sauvegarde trouvée.")
