87fafd34d7cee2d6b46dddf26fb147c2aaba1aab
Portono <evan.miniscloux@gmail.com>
2026-03-16 10:58:31 +0100

    g mis les nouvelles upgrades

--- sources/save_load.py
+++ sources/save_load.py
@@ -2,89 +2,68 @@ import json
 import os
 
 dico_all = {
-    #-------------------Upgrades stats classiques-------------------------------------
-    "dico_upgrades_stats":{
-        "chance": 1,
-        "gain_xp": 0,
-        "pv": 0,
-        "vitesse": 0,
-        "regen_pv": 0,
-        "vol_de_vie": 0,
-        "esquive": 0,
-        "renvoi": 0,
-    },
+    dico_upgrades_stats={
+                    "chance":1,         
+                    "gain_xp":0,        
+                    "pv":0,             
+                    "vitesse":0,                        
+                    "regen_pv":0,           
+                    "vol_de_vie":0,  
+                    "esquive":0
+                    },
 
-    #------------------Upgrades armes uniques----------------------------------------
-    "dico_upgrades_uniques": {
-        "laser": {
-            "laser_electrique": False,
-            "laser_ralentissant": False,
-            "laser_perforant": False
-        },
-        "roquette": {
-            "roquette_shrapnel": False,
-            "roquette_enflammee": False,
-            "roquette_ricochet": False
-        },
-        "mine": {
-            "mine_empoisonnee": False,
-            "mine_fragmentation": False,
-            "mine_double_vie": False
-        },
-        "aura": {
-            "aura_trainee": False,
-            "aura_scaling": False,
-            "aura_pulse": False
-        },
-        "tourelle": {
-            "tourelle_explosive": False,
-            "tourelle_aoe_defensive": False,
-            "tourelle_leurre": False
-        },
-    },
-
-    #------------------Upgrades stats laser------------------------------------------
-    "dico_upgrades_laser": {
-        "cadence_de_tir": 0,
-        "degat": 0,
-        "taille_projectile": 0,
-        "vitesse_balles": 0,
-        "portee": 0,
-    },
-
-    #------------------Upgrades stats roquette------------------------------------------
-    "dico_upgrades_roquette": {
-        "cadence_de_tir": 0,
-        "degat": 0,
-        "taille_projectile": 0,
-        "vitesse_balles": 0,
-        "portee": 0,
-    },
-
-    #------------------Upgrades stats mine------------------------------------------
-    "dico_upgrades_mine": {
-        "cadence_de_tir": 0,
-        "degat": 0,
-        "taille_projectile": 0,
-        "portee": 0
-    },
-
-    #------------------Upgrades stats aura------------------------------------------
-    "dico_upgrades_aura": {
-        "cadence_de_tir": 0,
-        "degat": 0,
-        "portee": 0
-    },
-
-    #------------------Upgrades stats tourelle------------------------------------------
-    "dico_upgrades_tourelle": {
-        "cadence_de_tir": 0,
-        "degat": 0,
-        "hp": 0,
-        "vitesse_balles": 0,
-        "portee": 0
+    dico_upgrades_uniques={
+                    "laser":{"laser_electrique":False,                  ##Fait
+                             "laser_ralentissant":False,                ##Fait
+                             "laser_perforant":False                    ##Fait
+                             },
+                    "roquette":{"roquette_shrapnel":False,              ##Fait
+                                "roquette_enflammee":False,             ##Fait
+                                "roquette_ricochet":False               ##Fait
+                                },
+                    "mine":{"mine_empoisonnee":False,                   ##Fait
+                            "mine_fragmentation":False,                 ##Fait
+                            "mine_double_vie":False                     ##Fait
+                            },
+                    "aura":{"aura_trainee":False,
+                            "aura_affaiblissante":False,                ##Fait
+                            "aura_pulse":False
+                            },
+                    "tourelle":{"tourelle_explosive":False,             ##Fait
+                                "tourelle_aoe_defensive":False,         ##Fait
+                                "tourelle_leurre":False                 ##Fait
+                                },
+                    },
+    dico_upgrades_laser={
+                    "cadence_de_tir":0,
+                    "degat":0,
+                    "portee":0,
+                    },   
+    dico_upgrades_roquette={
+                    "cadence_de_tir":0,
+                    "degat":0,
+                    "portee":0,
+                    "rayon_aoe":0,
+                    },
+    dico_upgrades_mine={
+                    "cadence_de_tir":0,
+                    "degat":0,
+                    "rayon_aoe":0,
+                    "duree_aoe":0,
+                    "duree_vie":0
+                    },
+    dico_upgrades_aura={
+                    "cadence_de_tir":0,
+                    "degat":0,
+                    "portee":0
+                    },
+    dico_upgrades_tourelle={
+                    "cadence_de_tir":0,
+                    "degat":0,
+                    "hp":0,
+                    "portee":0
+                    }
     }
-}
 
 
 def sauvegarder_jeu():
@@ -119,4 +98,5 @@ def charger_jeu():
         print("Sauvegarde chargée !")
     else:
         print("Aucune sauvegarde trouvée.")
+
 
