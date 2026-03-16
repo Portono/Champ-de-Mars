50add2524f18627591b8c1554eb708da49328c9f
Oltorc <kolkien2003@gmail.com>
2026-03-16 11:29:38 +0100

    Update save_load.py

--- sources/save_load.py
+++ sources/save_load.py
@@ -1,51 +1,46 @@
 import json
 import os
-from random_module import *
-from Main_game import *
-
-dico_all = {
-    "dico_upgrades_stats":dico_upgrades_stats,
-    "dico_upgrades_uniques":dico_upgrades_uniques,
-    "dico_upgrades_laser":dico_upgrades_laser,
-    "dico_upgrades_roquette":dico_upgrades_roquette,
-    "dico_upgrades_mine":dico_upgrades_mine,
-    "dico_upgrades_aura":dico_upgrades_aura,
-    "dico_upgrades_tourelle":dico_upgrades_tourelle,
-    "armes_possedees":armes_possedees
-    }
 
 
-def sauvegarder_jeu():
-    with open("save.json", "w") as fichier:
-        json.dump(dico_all, fichier, indent=4)
+SAVE_FILE = "save.json"
+
+
+def sauvegarder_jeu(donnee_de_sauvegarde):
+
+    with open(data_path(SAVE_FILE), "w") as fichier:
+        json.dump(donnee_de_sauvegarde, fichier, indent=4)
+
     print("Jeu sauvegardé !")
 
 
 def charger_jeu():
-    global pv_joueur, xp, niveau
-    global player_x, player_y
-    global nombre_journees, duree_journee
-    global type_armes
 
-    if os.path.exists("save.json"):
-        with open("save.json", "r") as fichier:
+    if os.path.exists(SAVE_FILE):
+
+        with open(SAVE_FILE, "r") as fichier:
             data = json.load(fichier)
 
-        pv_joueur = data.get("pv_joueur", 100)
-        xp = data.get("xp", 0)
-        niveau = data.get("niveau", 1)
+        Main_game.pv_joueur = data.get("pv_joueur", 100)
+        Main_game.xp = data.get("xp", 0)
+        Main_game.niveau = data.get("niveau", 1)
 
-        # Assure-toi que width et height sont définis globalement quelque part avant cet appel
-        player_x = data.get("player_x", width // 2)
-        player_y = data.get("player_y", height // 2)
+        Main_game.player_x = data.get("player_x", Main_game.width // 2)
+        Main_game.player_y = data.get("player_y", Main_game.height // 2)
 
-        nombre_journees = data.get("nombre_journees", 0)
-        duree_journee = data.get("duree_journee", 0)
+        Main_game.nombre_journees = data.get("nombre_journees", 0)
+        Main_game.duree_journee = data.get("duree_journee", 0)
 
-        type_armes = data.get("armes", [])
+        Main_game.armes_possedees = data.get("armes", [])
+
+        Main_game.dico_upgrades_stats = data.get("dico_upgrades_stats", {})
+        Main_game.dico_upgrades_uniques = data.get("dico_upgrades_uniques", {})
+        Main_game.dico_upgrades_laser = data.get("dico_upgrades_laser", {})
+        Main_game.dico_upgrades_roquette = data.get("dico_upgrades_roquette", {})
+        Main_game.dico_upgrades_mine = data.get("dico_upgrades_mine", {})
+        Main_game.dico_upgrades_aura = data.get("dico_upgrades_aura", {})
+        Main_game.dico_upgrades_tourelle = data.get("dico_upgrades_tourelle", {})
 
         print("Sauvegarde chargée !")
+
     else:
         print("Aucune sauvegarde trouvée.")
-
-
